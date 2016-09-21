4fee98a49d6c26564b98369ff1380eed4af8cbb3
Steven Hugg <hugg@fasterlight.com>
2016-09-21 12:09:03 -0400

    Javatari.getOpcodeMetadata

--- src/main/atari/cpu/M6502.js
+++ src/main/atari/cpu/M6502.js
@@ -32,7 +32,6 @@ jt.M6502 = function() {
         this.setRDY(true);
     };
 
-
     // Interfaces
     var bus;
     var RDY = false;
@@ -1751,6 +1750,32 @@ jt.M6502 = function() {
             jt.Util.message(text);
         }
     };
+
+    var opticks = [
+        // 0 1 2 3 4 5 6 7 8 9 a b c d e f
+        7, 6, 2, 8, 2, 3, 5, 5, 3, 3, 2, 3, 2, 4, 6, 6, 2, 5, 2, 8, 2, 4,
+        6, 6, 2, 4, 2, 7, 2, 4, 7, 7, 6, 6, 2, 8, 3, 3, 5, 5, 4, 3, 2, 3,
+        4, 4, 6, 6, 2, 5, 2, 8, 2, 4, 6, 6, 2, 4, 2, 7, 2, 4, 7, 7, 6, 6,
+        2, 8, 2, 3, 5, 5, 3, 3, 2, 2, 3, 4, 6, 6, 2, 5, 2, 8, 2, 4, 6, 6,
+        2, 4, 2, 7, 2, 4, 7, 7, 6, 6, 2, 8, 2, 3, 5, 5, 4, 3, 2, 2, 5, 4,
+        6, 6, 2, 5, 2, 8, 2, 4, 6, 6, 2, 4, 2, 7, 2, 4, 7, 7, 2, 6, 2, 6,
+        2, 2, 2, 3, 2, 2, 2, 2, 4, 4, 4, 4, 2, 6, 2, 6, 4, 4, 4, 4, 2, 5,
+        2, 5, 5, 5, 5, 5, 3, 6, 3, 6, 3, 3, 3, 3, 2, 3, 2, 2, 4, 4, 4, 4,
+        2, 5, 2, 5, 4, 4, 4, 4, 2, 4, 2, 4, 4, 4, 4, 5, 3, 6, 2, 8, 3, 3,
+        5, 5, 2, 3, 2, 2, 4, 4, 6, 6, 2, 5, 2, 8, 2, 4, 6, 6, 2, 4, 2, 7,
+        2, 4, 7, 7, 3, 6, 2, 8, 3, 3, 5, 5, 2, 3, 2, 3, 4, 4, 6, 6, 2, 5,
+        2, 8, 2, 4, 6, 6, 2, 4, 2, 7, 2, 4, 7, 7 ];
+
+    Javatari.getOpcodeMetadata = function(opcode, address) {
+      // TODO: more intelligent maximum cycles
+      var i = instructions[opcode];
+      return {
+        opcode:opcode,
+        mnenomic:opcodes[opcode],
+        minCycles:opticks[opcode],
+        maxCycles:i.length-1,
+      };
+    }
 
     //noinspection JSUnusedGlobalSymbols
     this.runCycles = function(cycles) {
